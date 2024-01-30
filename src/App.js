99b9c9a72900949233c5d07016abcc9e36c58a87
k941122 <941122k@naver.com>
2024-01-30 19:02:01 +0900

    Feat: DiaryList/DiaryItem 컴포넌트 제작 및 css작업

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
 import DiaryEdiror from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+const dummyList = [
+  {
+    id:1,
+    author:'차은우',
+    content:'하이 1',
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id:2,
+    author:'우도환',
+    content:'하이 2',
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id:3,
+    author:'임시완',
+    content:'하이 3',
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+]
 
 function App() {
   return (
     <div className="App">
       <DiaryEdiror/>
+      <DiaryList diaryList={dummyList}/>
     </div>
   );
 }
